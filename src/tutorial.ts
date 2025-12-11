a2af30944d6c098ba54695011a1099359d9458af
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-11 19:49:33 +0600

    feat : learn array

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -9,10 +9,12 @@ user = "Fahim"
 let Marks:number[] = [14,18,41,14,19,23,16,15.81,0.2];
 // marstdMarksks = [85,14,55,78,26,25,544,];
 
+// ==================   Topic => array              =====================
+
 let names:string[] = ["Hasib" ,"Fahim", "Rifat", "Hasan"];
 names = ["Akash" , "Shamim" , "Mahbub"];
-
-
+let arr:Array<number>
+arr=[15,15,1859]
 let isOnline:boolean;
 
 // isOnline= "Hi"
@@ -23,4 +25,10 @@ isOnline = true;
 // ======  Tuple ============
 
 let twoWord :[string,boolean] ;
-twoWord=["Hasib" , true]
+twoWord=["Hasib" , true];
+
+
+enum role {
+    "user" ,  "admin"
+}
+// role = "Hi"
